6af5641ddda0268955816c406fbcc8da556df44e
copokbl <zaneharrison2006@icloud.com>
2022-08-02 17:01:10 +1000

    Added funcs

--- Data/Schemas/Poll.cs
+++ Data/Schemas/Poll.cs
@@ -3,10 +3,10 @@ namespace DemocracyBot.Data.Schemas;
 public class Poll {
     
     /// <summary>
-    /// Current votes, (Runner, Vote Count)
+    /// (User who voted, who they voted for)
     /// </summary>
-    public Dictionary<long, int> Votes { get; set; }
-    
+    public Dictionary<ulong, ulong> Votes { get; set; }
+
     /// <summary>
     /// When the poll started
     /// </summary>
@@ -16,5 +16,21 @@ public class Poll {
     /// When the election will be called
     /// </summary>
     public long PollEnd { get; set; }
+    
+    public ulong GetWinner() {
+        int max = GetVotesCount().Max(kvp => kvp.Value);
+        return GetVotesCount().First(kvp => kvp.Value == max).Key;
+    }
+    
+    public Dictionary<ulong, int> GetVotesCount() {
+        Dictionary<ulong, int> votes = new();
+        foreach (ulong vote in Votes.Values) {
+            if (!votes.ContainsKey(vote)) {
+                votes.Add(vote, 0);
+            }
+            votes[vote]++;
+        }
+        return votes;
+    }
     
 }
--- Data/ServiceManager.cs
+++ Data/ServiceManager.cs
@@ -1,9 +1,9 @@
 namespace DemocracyBot.Data; 
 
 public static class ServiceManager {
-    
+
     public static void Init() {
-        TruthOrDareService.Init();
+        
     }
     
 }
